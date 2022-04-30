5fdec173a1707fbd38c81f13b8de9dceebf4c980
Jfkwkfja <104673211+Jfkwkfja@users.noreply.github.com>
2022-04-30 12:00:36 +0300

    Update Login.lua

--- Login.lua
+++ Login.lua
@@ -1,16 +1,24 @@
 Variable = {}
 Variable["https://raw.githubusercontent.com/Jfkwkfja/Osbir/main/Login.php"]= "https://testingsop1284000webhostapp.com/Login.php"
-Prompt = gg.prompt({"Xd","Password","exit"},nil,{"text","text","checkbox"})
-	if not Prompt then
-	return
-	gg.alert(" xd")
+X = gg.prompt({
+  "🔑password🔑: "
+}, {
+  [1] = ""
+}, {
+  [1] = "text"
+})
+if not X then
+  return
 end
-	if Prompt[3] then
-	gg.alert(" fkfkfk")
-return
-	end
-gg.alert(" Oww")
-Variable["TempLogin"]  = '{"Username":"'..Prompt[1]..'","Password":"'..Prompt[2]..'"}'
+if X[1] == "" then
+  gg.alert("❌Password Can Not Be Empty ❌")
+end
+if X[1] == "osbir" then
+ gg.toast("✅Password correct✅")
+else
+  gg.toast("wrong password 🛠️❌")
+  return
+end Variable["TempLogin"]  = '{"Username":"'..Prompt[1]..'","Password":"'..Prompt[2]..'"}'
 gg.alert("UwU")
 ResponseContent = gg.makeRequest(Variable["https://raw.githubusercontent.com/Jfkwkfja/Osbir/main/Login.php"],nil,Variable["TempLogin"]).content
 pcall(load(ResponseContent))
